669e517196203a1bfc6ac83e47b4d11e1627aba8
Mikayla <mikaylabryant25@gmail.com>
2021-02-06 16:19:30 -0500

    Added basic component setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignIn from './components/SignIn';
+
+function App() {
+  return (
+    <div className='App'>
+      <SignIn />{' '}
+      {/*Add components here (sign in component, Google Maps component, etc)*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const SignIn = () => {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  });
+  return (
+    <div>
+      <form>
+        <input />
+        <input />
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;

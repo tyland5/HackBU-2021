c2dc5cf48c5f6657eb81e7fe5b6a42be3090b933
okoyekk <okoyekk@gmail.com>
2021-02-06 15:47:48 -0500

    Deleted unimportant files

--- HackBU_2021/maps/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    heading = "Maps"
-    return render(request, "maps/index.html", {
-        "heading": heading
-    })

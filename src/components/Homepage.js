8e79f67bda689902e55c6932495554dcbf188d90
Mikayla <mikaylabryant25@gmail.com>
2021-02-07 08:50:44 -0500

    Re-added deleted files

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <h1>Find the perfect gift.</h1>
+      <div className='search'>
+        <p>Generate new idea {`>>`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;

aa1c55cdc1cbee4dbf72328559fdedf815ca040e
Mikayla <mikaylabryant25@gmail.com>
2021-02-06 20:27:26 -0500

    Added gif to background and temporary styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import SignIn from './components/SignIn';
-import Example from './components/Example';
+import Homepage from './components/Homepage';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
-      <SignIn />
-      {/*Add components here (sign in component, Google Maps component, etc)*/}
-      <Example />
+      <Homepage />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <h1>Find the perfect gift.</h1>
+      <div>
+        <p>Search Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
